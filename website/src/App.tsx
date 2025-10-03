998e0c8c030338783d9d2ac08e216b8e4f624b47
devashree pathak <devashreepathak22@gmail.com>
2025-10-03 12:58:59 +0530

    web init

---
+++ website/src/App.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div className='h-screen w-full bg-black text-white flex items-center justify-center'>App</div>
+  )
+}
+
+export default App
